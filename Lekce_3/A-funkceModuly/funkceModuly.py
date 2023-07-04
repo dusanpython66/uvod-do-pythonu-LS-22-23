28809730d27ce911b503df83e5076e2477abaee5
cls <dusanbednarik@gmail.com>
2023-07-04 20:50:45 +0200

    new commit

--- Lekce_3/A-funkceModuly/funkceModuly.py
+++ Lekce_3/A-funkceModuly/funkceModuly.py
@@ -7,37 +7,46 @@ def pozdrav():
     prijmeni = "novák"
     print(f"Pan {jmeno} {prijmeni}")  # f-string (formátovaný řetězec)
 
+
 # Nyní zavoláme funkci pozdrav()
 pozdrav()
+
 
 def pozdrav(jmeno, prijmeni):
     print(f"Pan/Paní {jmeno} {prijmeni}")
 
-pozdrav(prijmeni="Kolář", jmeno = "Petr")
+
+pozdrav(prijmeni="Kolář", jmeno="Petr")
 
 print("Karel Novák", "Petr Kolář", sep="     ")  # sep je parametr funkce print (oddělovač) - defaultně je mezera
+
 
 # defaultní hodnoty parametrů
 def pozdrav(jmeno="Karel", prijmeni="Novák"):
     print(f"Pan/Paní {jmeno} {prijmeni}")
 
+
 pozdrav()
+
 
 # funkce s parametry
 
 def prepona(a, b):
-    c = (a**2 + b**2)**(1/2)
+    c = (a ** 2 + b ** 2) ** (1 / 2)
     print("c = ", c)
 
+
 prepona(3, 4)
+
 
 # funkce vracející hodnotu:
 
 def prepona(a, b):
-    c = (a**2 + b**2)**(1/2)
+    c = (a ** 2 + b ** 2) ** (1 / 2)
     return c  # vrací hodnotu proměnné c
 
-delkaPrepony = prepona(3, 4) # uložíme si hodnotu proměnné c do proměnné delkaPrepony
+
+delkaPrepony = prepona(3, 4)  # uložíme si hodnotu proměnné c do proměnné delkaPrepony
 
 # print(prepona(3, 4) * 2)
 print("Délka přepony je rovna: ", delkaPrepony)
@@ -50,10 +59,11 @@ print(f"Délka přepony je rovna: {delkaPrepony}")
 # Více o modulu Sympy se můžete dočíst v dokumentaci. https://docs.sympy.org/latest/index.html
 
 import sympy as sym
+
 sym.sin(sym.pi)
 
 x = sym.symbols("x")
-f = sym.sin((2*x) / (x**2 + 1))
+f = sym.sin((2 * x) / (x ** 2 + 1))
 print(sym.diff(f, x))
 
 #######  externí moduly vytvořené uživatelem
@@ -65,31 +75,86 @@ sys.path.append('./Lekce_2/A-funkceModuly')
 from mujModul import delkaPrepony  # import uživatelského modulu
 
 # volání funkce z uživatelského modulu:
-c = delkaPrepony(3,20)
+c = delkaPrepony(3, 20)
 print(c)
 
 print(1 / 0)
 
+
 # handling exceptions  (zachytávání výjimek)
 # Syntaxe:
-# try:
+# try:   # povinný blok  (pokud dojde k vyvolání výjimky, přeskočí se na blok except)
 #     <kód>
-# except <výjimka>:
+# except <výjimka>:    # nepovinný blok  (pokud dojde k vyvolání výjimky, vykoná se tento blok)
 #     <kód>
-# else:
+# else:   # nepovinný blok  (vykoná se pokud nedošlo k vyvolání výjimky)
 #     <kód>
 # finally:   # nepovinný blok  (vykoná se vždy)
 #     <kód>
 
 
 def prepona(a, b):
-    try: # pokusí se provést následující kód
-        c = (a**2 + b**2)**(1/2)
-    except TypeError: # pokud dojde k vyvolání výjimky TypeError
+    try:  # pokusí se provést následující kód
+        c = (a ** 2 + b ** 2) ** (1 / 2)
+    except TypeError:  # pokud dojde k vyvolání výjimky TypeError
         print("Funkce přijímá pouze čísla")
     else:  # pokud nedošlo k vyvolání výjimky
         return c
-prepona(4, 3)
+
+
+prepona("a", 3)
+
+
+# Ukažme příklad s použitím klíčového slova finally.
+
+def prepona(a, b):
+    try:
+        c = (a ** 2 + b ** 2) ** (1 / 2)
+    except TypeError:
+        print("Funkce přijímá pouze čísla")
+    else:  # pokud nedošlo k vyvolání výjimky
+        return c
+    finally:
+        print("Toto se vypíše vždy")
+
+
+prepona("aů", 3)
+
+
+def vydel(num1, num2):
+    """
+    Funkce vydel() vrací výsledek dělení dvou čísel.
+    Pokud je dělitel nula, vypíše se chybová hláška a funkce vrací None.
+    """
+    try:
+        result = num1 / num2
+    except ZeroDivisionError:
+        print("Nelze dělit nulou.")
+        return None  # None je speciální hodnota, která značí prázdnou hodnotu
+    else:
+        print(f"Výsledek dělení je {result}.")
+        return result
+    finally:
+        print("Funkce vydel() byla ukončena.")
+
+
+# Příklad použití
+result = vydel(10, 0)
+
+
+# Výjimky můžeme vyvolávat i sami pomocí klíčového slova raise.
+# Syntaxe:
+# raise <výjimka>(<parametry>)
+# Příklad:
+
+def prepona(a, b):
+    if type(a) != int or type(b) != int:
+        raise TypeError("Funkce přijímá celá nebo desetinná čísla")
+    c = (a ** 2 + b ** 2) ** (1 / 2)
+    return c
+
+
+prepona(3.14, 4)
 
 
 # Collatzova posloupnost  (https://en.wikipedia.org/wiki/Collatz_conjecture)
@@ -104,10 +169,12 @@ def collatz(number):
     else:
         return 3 * number + 1
 
+
 n = int(input("Zadej číslo: "))
 while n != 1:
     n = collatz(n)  # přiřazení výsledku funkce collatz() do proměnné n
     print(n)
+
 
 # Předávání libovolného počtu parametrů do funkce
 # Syntaxe:
@@ -120,6 +187,7 @@ while n != 1:
 def udelej_pizzu(*ingredience):
     print("Ingredience: ", ingredience)
 
+
 # volání funkce udelej_pizzu()
 udelej_pizzu("sýr", "šunka", "rajče", "houby")
 
@@ -131,6 +199,7 @@ udelej_pizzu("sýr", "šunka", "rajče", "houby")
 def udelej_pizzu(velikost, *ingredience):
     print("Velikost: ", velikost)
     print("Ingredience: ", ingredience)
+
 
 udelej_pizzu("velká", "sýr", "šunka", "rajče", "houby")
 

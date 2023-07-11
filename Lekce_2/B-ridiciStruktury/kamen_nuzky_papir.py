5ea741ca854d2013fcaf940fe5283837abf7cb91
cls <dusanbednarik@gmail.com>
2023-07-11 21:20:57 +0200

    starting kamen_nuzky_papir.py coding

--- Lekce_2/B-ridiciStruktury/kamen_nuzky_papir.py
+++ Lekce_2/B-ridiciStruktury/kamen_nuzky_papir.py
@@ -1,1 +1,25 @@
 # hra "Kámen nůžky papír"
+
+# import random
+import random
+
+# Zvolme počet vítězných kol
+pocetViteznychKol = int(input("Zadej počet vítězných kol: "))
+
+# Vytvořme seznam možných tahů
+# tahy = ["kámen", "nůžky", "papír"]
+
+while True:
+    # Zadej tah
+
+    try:
+        tah = input("Zadej svůj tah, 'k' = kámen, 'n' = nůžky a 'p' = papír: ")
+
+        if tah != "k" and tah != "n" and tah != "p":
+            raise ValueError("Tah není platný")
+    except ValueError as err:
+        print(err)
+
+    else:
+        pass  # pokračujeme dále v kódu
+        break
